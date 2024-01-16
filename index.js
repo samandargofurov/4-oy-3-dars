ff321702aab5572ecec9a25a4b11dc74fe7c0803
samandargofurov <samandargofurov236@gmail.com>
2024-01-16 23:40:06 +0500

    lesson 3 homework

---
+++ index.js
@@ -0,0 +1,92 @@
+// 1. 3 ta son berilgan, bular orasidan 3 ga karralilarining kopaytmasini topuvchi algoritm yozing
+
+// let a = 3;
+// let b = 9;
+// let c = 10;
+// let sum = 1;
+
+// if (a % 3 === 0) {
+//     sum *= a;
+// }
+// if (b % 3 === 0) {
+//     sum *= b;
+// }
+// if (c % 3 === 0) {
+//     sum *= c;
+// }
+// if (sum === 1){
+//     sum = 0
+// }
+// console.log(sum);
+
+// 2. 3 xonali son berilgan. Uning raqamlari yigindisini topuvchi algoritm yozing.
+
+// let num = 123;
+
+// let birlik = num % 10;
+// let on = ((num - birlik) / 10) % 10;
+// let yuzlik = (num - on * 10 - birlik) / 100;
+// let res = (yuzlik + on + birlik);
+// console.log(res);
+
+// 3. 3 xonali son berilgan uning toq raqamlari kopaytmasini topuvchi algoritm yozing
+
+// let num = 723;
+// let sum = 1;
+
+// let birlik = num % 10;
+// let on = ((num - birlik) / 10) % 10;
+// let yuzlik = (num - on * 10 - birlik) / 100;
+
+// if (birlik % 2 == 1) {
+//     sum *= birlik;
+// }
+// if (on % 2 == 1) {
+//     sum *= on;
+// }
+// if (yuzlik % 2 == 1) {
+//     sum *= yuzlik;
+// }
+
+// console.log(sum);
+
+// 4. 3 ta son berilgan ular orasidan 5 ga hamda 7 ga karralilarining sonini topuvchi algoritm yozing
+
+// let a = 15;
+// let b = 35;
+// let c = 70;
+// let counter = 0;
+// if (a % 5 == 0 && a % 7 == 0){
+//     counter++;
+// }
+// if (b % 5 == 0 && b % 7 == 0){
+//     counter++;
+// }
+// if (c % 5 == 0 && c % 7 == 0){
+//     counter++;
+// }
+
+// console.log(counter);
+
+// 5. Ixtiyoriy harf berilganda uning unli yoki undosh ekanini aniqlovchi algoritm yozing
+
+// let word = "p";
+// let unli = "a, e, i, u, o"
+
+// if (word == unli) {
+//     res = "undosh harf"
+// } else{
+//     res = "unli harf"
+// }
+
+// console.log(res);
+
+// 6. Foydalanuvchidan 2 ta son hamda bitta arifmetik operator qabul qiladigan hamda arfimetik operatorga qarab amal bajaradigan
+// calculator yarating.
+// (Masalan 1-son 2 ga 2-son 3 ga va operator + ga teng bolsa natija 5 chiqishi kerak)
+
+
+// 7. 3 ta musbat son berilgan. Ushbu 3 ta sondan uchburchak yasash mumkin yoki mumkin emasligini aniqlovchi algoritm yozing. (Geometriyaga oid)
+
+
+// 8. 3 xonali son berilgan. Uning palindrom yoki palindrom emasligini tekshriruvchi algoritm yozing
